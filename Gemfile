53a07cfb6221b2cbdb04500cc6271e689317d003
Jesse Wolgamott <jesse@comalproductions.com>
2015-04-20 13:17:36 -0500

    Remote bitly: twitter can auto-shrink urls using t.co

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-gem 'bitly', '>0.5'
 gem 'twitter'
 gem 'pry'
 gem 'dotenv'
--- builds_tweet.rb
+++ builds_tweet.rb
@@ -1,17 +1,6 @@
 class BuildsTweet
 
-  def initialize
-    Bitly.use_api_version_3
-
-    Bitly.configure do |config|
-      config.api_version = 3
-      config.access_token = ENV.fetch("BITLY_ACCESS_TOKEN")
-    end
-  end
-
   def tweet(quote_data)
-    shortened_url = Bitly.client.shorten(quote_data.article_url).short_url
-
     quote_with_url = [quote_data.quote, quote_data.article_url].join(" ")
     post  = client.update_with_media quote_with_url, File.new(quote_data.file_path)
     tweet = client.status post.id
